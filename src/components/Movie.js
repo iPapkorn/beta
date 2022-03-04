d73612cc3eec682872f73e4aa3d8ff22110ffd7a
Jayant Rohila <jrohila55@gmail.com>
2022-03-04 16:37:36 +0530

    Added React Support
    and material ui

---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Movie = () => (
+	<div className="Movie">
+		<h1>Movie component</h1>
+	</div>
+);
+export default Movie;
---
+++ src/components/header/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./header.css";
+const header = () => {
+	return <span className="header"> ipapkorn </span>;
+};
+
+export default header;
